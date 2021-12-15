392d56b538dbf42298ad2b033eb4bc0856bcdb6a
Marty Mazurik <marty@martymazurik.com>
2021-12-15 11:04:58 -0800

    initial check-in

---
+++ pdt-svc/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pdt_svc.Controllers
+{
+    [Route("[controller]")] //   /admin
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        // GET:  /ping
+        [HttpGet("ping")]
+        public IActionResult Get()
+        {
+            return Ok("200 OK Ping");
+        }
+    }
+}
