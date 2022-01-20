382ec2f7af31aad7395dce831d06470b286dbee2
Marty Mazurik <marty@martymazurik.com>
2022-01-20 14:11:03 -0800

    see if bogusException() show in Google Error Reporting

---
+++ pdt.svc/services/exceptions/bogusException.cs
@@ -0,0 +1,7 @@
+﻿namespace pdt.svc.services.exceptions
+{
+    public class bogusException : Exception      // read more on user-defined exceptions here: https://docs.microsoft.com/en-us/dotnet/standard/exceptions/how-to-create-user-defined-exceptions
+    {
+        public bogusException(string message) : base(message) { }
+    }
+}
